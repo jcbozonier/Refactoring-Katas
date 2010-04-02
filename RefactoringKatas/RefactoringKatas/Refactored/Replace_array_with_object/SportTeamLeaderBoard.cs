47704f30bbba0cafc1a64c4db819d3c217098428
unknown <Justin.Bozonier@.corp.seat.milliman.com>
2010-04-01 18:00:00 -0700

    completed the refactoring of the Replace array with object kata.

--- RefactoringKatas/RefactoringKatas/Refactored/Replace_array_with_object/SportTeamLeaderBoard.cs
+++ RefactoringKatas/RefactoringKatas/Refactored/Replace_array_with_object/SportTeamLeaderBoard.cs
@@ -4,23 +4,9 @@ using System.Linq;
 
 namespace RefactoringKatas.Refactored.Replace_array_with_object
 {
-    public class Performance
-    {
-        public Performance(string name, string wins, string losses)
-        {
-            Name = name;
-            Wins = wins;
-            Losses = losses;
-        }
-
-        public string Name { get; private set; }
-        public string Wins { get; private set; }
-        public string Losses { get; private set; }
-    }
-
     public class SportTeamLeaderBoard
     {
-        private readonly Performance[] TeamData;
+        private readonly IEnumerable<Performance> TeamData;
 
         public SportTeamLeaderBoard(params Performance[] teamData)
         {
