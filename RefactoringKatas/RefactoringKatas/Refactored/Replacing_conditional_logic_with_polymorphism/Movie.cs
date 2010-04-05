4ee16072e5def32a581680f2d7a0d037d7c9794d
unknown <Justin.Bozonier@.corp.seat.milliman.com>
2010-04-05 11:04:51 -0700

    Moved the pricing logic into the movie object. Getting it closer to the pricing objects.

--- RefactoringKatas/RefactoringKatas/Refactored/Replacing_conditional_logic_with_polymorphism/Movie.cs
+++ RefactoringKatas/RefactoringKatas/Refactored/Replacing_conditional_logic_with_polymorphism/Movie.cs
@@ -1,3 +1,4 @@
+using System;
 using RefactoringKatas.Refactored.Replacing_conditional_logic_with_polymorphism.Pricing;
 
 namespace RefactoringKatas.Refactored.Replacing_conditional_logic_with_polymorphism
@@ -14,6 +15,59 @@ namespace RefactoringKatas.Refactored.Replacing_conditional_logic_with_polymorph
         public MovieRentalPriceCode GetPriceCode()
         {
             return PriceCode.GetPriceCode();
+        }
+
+        public void Charge(IPayingCustomer customer, int DaysRented)
+        {
+            switch (GetPriceCode())
+            {
+                case MovieRentalPriceCode.Regular:
+                    {
+                        var rentalCostPerDay = 2;
+                        var allowedRentalPeriod = 2;
+
+                        if (DaysRented > allowedRentalPeriod)
+                        {
+                            var lateRentalDays = DaysRented - allowedRentalPeriod;
+                            var lateRentalPercentageIncrease = 1.5;
+                            var totalRentalFeeIncludingLateFee = allowedRentalPeriod * rentalCostPerDay +
+                                                                 lateRentalDays * rentalCostPerDay *
+                                                                 lateRentalPercentageIncrease;
+
+                            customer.Charge(totalRentalFeeIncludingLateFee);
+                        }
+                        else
+                        {
+                            var totalRentalFee = DaysRented * rentalCostPerDay;
+                            customer.Charge(totalRentalFee);
+                        }
+                        break;
+                    }
+                case MovieRentalPriceCode.NewRelease:
+                    {
+                        var rentalCostPerDay = 3;
+                        var totalRentalFee = rentalCostPerDay * DaysRented;
+                        customer.Charge(totalRentalFee);
+                        break;
+                    }
+                case MovieRentalPriceCode.Childrens:
+                    {
+                        var rentalCostPerDay = 1.5;
+                        var allowedRentalPeriod = 3;
+                        if (DaysRented > allowedRentalPeriod)
+                        {
+                            var daysLate = DaysRented - allowedRentalPeriod;
+                            customer.Charge(rentalCostPerDay * allowedRentalPeriod + daysLate * rentalCostPerDay * 1.5);
+                        }
+                        else
+                        {
+                            var totalRentalFee = rentalCostPerDay * DaysRented;
+                            customer.Charge(totalRentalFee);
+                        }
+
+                        break;
+                    }
+            }
         }
     }
 }
--- RefactoringKatas/RefactoringKatas/Refactored/Replacing_conditional_logic_with_polymorphism/MovieRental.cs
+++ RefactoringKatas/RefactoringKatas/Refactored/Replacing_conditional_logic_with_polymorphism/MovieRental.cs
@@ -20,55 +20,7 @@ namespace RefactoringKatas.Refactored.Replacing_conditional_logic_with_polymorph
 
         public void Charge(IPayingCustomer customer)
         {
-            switch (TheMovie.GetPriceCode())
-            {
-                case MovieRentalPriceCode.Regular:
-                    {
-                        var rentalCostPerDay = 2;
-                        var allowedRentalPeriod = 2;
-
-                        if (DaysRented > allowedRentalPeriod)
-                        {
-                            var lateRentalDays = DaysRented - allowedRentalPeriod;
-                            var lateRentalPercentageIncrease = 1.5;
-                            var totalRentalFeeIncludingLateFee = allowedRentalPeriod*rentalCostPerDay +
-                                                                 lateRentalDays*rentalCostPerDay*
-                                                                 lateRentalPercentageIncrease;
-
-                            customer.Charge(totalRentalFeeIncludingLateFee);
-                        }
-                        else
-                        {
-                            var totalRentalFee = DaysRented*rentalCostPerDay;
-                            customer.Charge(totalRentalFee);
-                        }
-                        break;
-                    }
-                case MovieRentalPriceCode.NewRelease:
-                    {
-                        var rentalCostPerDay = 3;
-                        var totalRentalFee = rentalCostPerDay*DaysRented;
-                        customer.Charge(totalRentalFee);
-                        break;
-                    }
-                case MovieRentalPriceCode.Childrens:
-                    {
-                        var rentalCostPerDay = 1.5;
-                        var allowedRentalPeriod = 3;
-                        if(DaysRented > allowedRentalPeriod)
-                        {
-                            var daysLate = DaysRented - allowedRentalPeriod;
-                            customer.Charge(rentalCostPerDay * allowedRentalPeriod + daysLate * rentalCostPerDay * 1.5);
-                        }
-                        else
-                        {
-                            var totalRentalFee = rentalCostPerDay * DaysRented;
-                            customer.Charge(totalRentalFee);
-                        }
-                    
-                        break;
-                    }
-            }
+            TheMovie.Charge(customer, DaysRented);
         }
     }
 }
