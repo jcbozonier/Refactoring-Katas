173180528053cd4039489da9f9ce37f222f2315f
unknown <Justin.Bozonier@.corp.seat.milliman.com>
2010-04-05 10:59:12 -0700

    removed the generic price class.

--- RefactoringKatas/RefactoringKatas/Refactored/Replacing_conditional_logic_with_polymorphism/Price.cs
+++ RefactoringKatas/RefactoringKatas/Refactored/Replacing_conditional_logic_with_polymorphism/Price.cs
@@ -2,21 +2,6 @@ using System;
 
 namespace RefactoringKatas.Refactored.Replacing_conditional_logic_with_polymorphism
 {
-    public class Price : IPrice
-    {
-        private readonly MovieRentalPriceCode PriceCode;
-
-        public Price(MovieRentalPriceCode priceCode)
-        {
-            PriceCode = priceCode;
-        }
-
-        public MovieRentalPriceCode GetPriceCode()
-        {
-            return PriceCode;
-        }
-    }
-
     public class RegularPrice : IPrice
     {
         #region Implementation of IPrice
